896530d3507b70caa94b9a1f22cd855d84c459c6
Stanislav-Zhuk <steys.ua@gmail.com>
2023-06-29 21:10:01 +0300

    stuled login and signup

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import LoginForm from 'components/LoginForm/LoginForm';
+import { Box, Container } from '@mui/material';
 
 const Login = () => {
   return (
-    <div>
-      <LoginForm />
-    </div>
+    <Container
+      component="div"
+      maxWidth="xs"
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          marginTop: 6,
+          padding: 4,
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+          borderRadius: '10px',
+          boxShadow: '0 5px 10px rgba(0,0,0,.3)',
+          webkitBoxShadow: '0 5px 10px rgba(0,0,0,.3)',
+          mozBoxShadow: '0 5px 10px rgba(0,0,0,.3)',
+        }}
+      >
+        <LoginForm />
+      </Box>
+    </Container>
   );
 };
 
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,11 +1,34 @@
 import React from 'react';
 import RegisterForm from 'components/RegisterForm/RegisterForm';
+import { Box, Container } from '@mui/material';
 
 const Register = () => {
   return (
-    <div>
-      <RegisterForm />
-    </div>
+    <Container
+      component="div"
+      maxWidth="xs"
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      }}>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          marginTop: 6,
+          padding: 4,
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+          borderRadius: '10px',
+          boxShadow: '0 5px 10px rgba(0,0,0,.3)',
+          webkitBoxShadow: '0 5px 10px rgba(0,0,0,.3)',
+          mozBoxShadow: '0 5px 10px rgba(0,0,0,.3)',
+        }}
+      >
+        <RegisterForm />
+      </Box>
+    </Container>
   );
 };
 
